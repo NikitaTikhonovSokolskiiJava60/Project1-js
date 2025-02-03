afbf8de91a4be62def0685946091c6648fd26d9e
Nikitikhonov <nikitikhonov@gmail.com>
2025-02-03 22:23:28 +0200

    end

--- src/script.js
+++ src/script.js
@@ -3,11 +3,24 @@ const detailedTitle = document.querySelector(".detailedContainer-title");
 const boxesElements = document.querySelectorAll(".cars-image");
 
 function setDetails(boxElement) {
-    detailedImage.alt = boxElement.getAttribute("data-alt-text");
+    detailedImage.src = boxElement.src;
     detailedTitle.innerHTML = boxElement.getAttribute("data-detailed-text");
 }
 for(let i = 0; i < boxesElements.length; i++) {
-    boxesElements[i].addEventListener("Click", function () {
+    boxesElements[i].addEventListener("click", function () {
         setDetails(boxesElements[i]);
+        detailedImage.classList.add('animate__animated', 'animate__fadeInBottomLeft');
+        detailedImage.style.setProperty('--animate-duration', '1s');
+        detailedImage.addEventListener("animationend", AnimationHandler, false);
+        function AnimationHandler () {
+            detailedImage.classList.remove('animate__animated', 'animate__fadeInBottomLeft');
+        }
+        detailedTitle.classList.add('animate__animated', 'animate__fadeInBottomLeft');
+        detailedTitle.style.setProperty('--animate-duration', '1s');
+        detailedTitle.addEventListener("animationend", AnimationHandler1, false);
+        function AnimationHandler1 () {
+            detailedTitle.classList.remove('animate__animated', 'animate__fadeInBottomLeft');
+        }
     });
 }
+document.documentElement.style.setProperty('')
